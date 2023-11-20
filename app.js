c5e2b3b7bdb472f03b941f1e3c8a487cadb1e774
Federico Fabre <100488524+fedefabre1@users.noreply.github.com>
2023-11-20 08:51:32 -0300

    "commit desde github"

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-let prueba = [a,b,c,d,e,f,g,h,i,x];
+let prueba = [a,b,,d,e,f,g,h,i,x];
 
-let prueba2= [ 1,2,3,4,5,6.7];
+let prueba2= [ 1,2,3,,5,6.7];
 
+let prueba3 = [ 10,12,13,14,16];
 
 console.log(prueba);
